bbd61f49ad5fee464a530385574d6fb74da3ebbf
Sarvarxo'ja Ismoilxo'jayev <103130081+sarvarxoja@users.noreply.github.com>
2025-02-16 17:55:14 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
-axios.defaults.baseURL = "https://admin.alsafia.uz/api"
+axios.defaults.baseURL = "https://alsafia.uz/api"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
